fca3dd3dbe2b7da1d2cdf139a2ebea5b43c3fee7
Paulo Leme <paulo.oph@hotmail.com>
2024-09-30 16:59:37 -0300

    Criado a conexão com a API e a tabela de exibição dos Alunos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
+import logoCadastro from '../src/assets/cadastro.png';
 
 function App() {
+
+  const baseUrl="https://localhost:7179/api/alunos";
+  
+  const [data, setData] = useState([]);
+
+  const pedidoGet = async () => {
+    await axios.get(baseUrl)
+    .then(response => {
+      setData(response.data);
+    }).catch(error=>{
+      console.log(error);
+    })
+  }
+
+  useEffect(() =>{
+    pedidoGet();
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Curso React com API Asp.NET Core</h1>
+      <br/>
+      <h3>Cadastro de Alunos</h3>
+      <header>
+        <img src={logoCadastro} alt="Cadastro"/>
+        <button className='btn btn-success'>Incluir Novo Aluno</button>
       </header>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Idade</th>
+            <th>Operação</th>
+          </tr>
+        </thead>
+        <tbody>
+            {data.map(aluno=>(
+              <tr key={aluno.id}>
+                <td>{aluno.id}</td>
+                <td>{aluno.nome}</td>
+                <td>{aluno.email}</td>
+                <td>{aluno.idade}</td>
+                <td>
+                  <button className="btn btn-primary">Editar</button> {" "}
+                  <button className="btn btn-danger">Exluir</button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 }
